1e4c8710cc365bdb2251154358c58e9f80ccddc3
solmakon6@gmail.com <eparis@atlassian.com>
2024-10-27 13:59:25 -0500

    Excercise 2

--- tic-tac-toe-master/tic-tac-toe.js
+++ tic-tac-toe-master/tic-tac-toe.js
@@ -5,13 +5,53 @@ document.addEventListener('DOMcontentLoaded', ()=>
 {
     //select all the div elements inside the game board
     const squares = document.querySelectorAll('#board div');
+    // starting player uses "x"
+    let currentPlayer = 'X';
+    // track the game progress
+    const gameState = Array(9).fill(null);
 
     //loop through the squares and add "square" class
     squares.forEach
-    (square =>
+    ((square, index) =>
 
         {
             square.classList.add('square');
         }
     );
 });
+
+
+// excercise 2
+
+document.addEventListener('DOMcontentLoaded', ()=>
+    {
+        //select all the div elements inside the game board
+        const squares = document.querySelectorAll('#board div');
+        // starting player uses "x"
+        let currentPlayer = 'X';
+        // track the game progress
+        const gameState = Array(9).fill(null);
+    
+        //loop through the squares and add "square" class
+        squares.forEach
+        ((square, index) =>
+        {
+            square.addEventListener('click', () =>)
+    
+            {
+                if (!gameState[index]) // CHECK IF THE SQUARE IS FILLED
+                {
+                    gameState[index] = currentPlayer; //RECORD THE PLAYER'S MOVE
+                    square.textContent = currentPlayer; // SHOW THE X OR O 
+                    square.classList.add(currentPlayer); //ADD CLASS FOR X OR O
+                    
+
+                    //TOGGLE TO THE OTHER PLAYER
+
+                    currentPlayer = currentPlayer === 'X' ? 'O' 'X'
+                }
+            }
+        }
+        );
+    
+    });
